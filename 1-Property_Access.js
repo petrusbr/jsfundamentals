70eb4db020235b83754cb3990c02e2f077645d61
Pedro Teixeira <pedrao@gmail.com>
2019-01-30 14:52:07 -0200

    Create 1-Property_Access.js

---
+++ 1-Property_Access.js
@@ -0,0 +1,23 @@
+let person = {};
+person.name = "Mr. Hanky";
+console.log(person.name);
+let pessoa = {
+  nome : "Mr. Teixeira"
+};
+console.log("pessoa.nome --> ", pessoa.nome);
+console.log("* Assigning pessoa.nome to variable who *");
+let who = pessoa.nome;
+console.log("who --> ", who);
+console.log("Assigning pessoa to another object");
+let pipo = pessoa;
+console.log("pipo.nome -->", pipo.nome);
+console.log("------------------------");
+console.log("* Changing pessoa.nome value *");
+pessoa.nome = "Lilo";
+console.log("pessoa.nome --> ", pessoa.nome);
+console.log("who --> ", who);
+console.log("pipo.nome --> ", pipo.nome);
+console.log("A variável who não teve seu valor alterado após a mudança no valor do atributo nome, porque \
+            valores primitivos (no caso, string) são passados por valor");
+console.log("Já o objeto pipo teve o valor do atributo alterado, pois objetos (valores não-primitivos) são passados \
+            como referência.");
