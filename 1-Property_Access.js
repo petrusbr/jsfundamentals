ad17ceb5f10a6b1b744e1dd026b269432ecfcd86
Pedro <pedrao@gmail.com>
2019-02-01 15:11:15 +0000

    Changed file name

--- 1-Property_Access.js
+++ 1-Property_Access.js
@@ -17,7 +17,5 @@ pessoa.nome = "Lilo";
 console.log("pessoa.nome --> ", pessoa.nome);
 console.log("who --> ", who);
 console.log("pipo.nome --> ", pipo.nome);
-console.log("A variável who não teve seu valor alterado após a mudança no valor do atributo nome, porque \
-            valores primitivos (no caso, string) são passados por valor");
-console.log("Já o objeto pipo teve o valor do atributo alterado, pois objetos (valores não-primitivos) são passados \
-            como referência.");
+console.log(`A variável who não teve seu valor alterado após a mudança no valor do atributo nome, porque valores primitivos (no caso, string)
+são passados por valor. Já o objeto pipo teve o valor do atributo alterado, pois objetos (valores não-primitivos) são passados como referência.`);
